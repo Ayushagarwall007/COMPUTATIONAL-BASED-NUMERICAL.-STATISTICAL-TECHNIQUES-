2b447db9f3575e7e83f5a1e11fbc136211c9cbd8
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2022-12-16 23:41:36 +0530

    Update EulerMethod.c

--- EulerMethod.c
+++ EulerMethod.c
@@ -1,30 +1,28 @@
+// euler Method
+
+
+
 #include<stdio.h>
-#include<stdlib.h>
 #include<math.h>
-float f(float x, float y){
-    return x+y;
-}
-
-
-int main(){
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-    float x0, y0;
-    printf("Enter x0 and y0: \n");
+#define f(x, y) (1-y)
+int main()
+{
+    float x0, y0, h, x;
+    printf("Enter the x0 and y0 : ");
     scanf("%f%f", &x0, &y0);
-    float xn,n;
-    printf("Enter n and xn: \n");
-    scanf("%f%f", &n,&xn);
-    float h = (xn-x0)/n;
-    float x=x0,y=y0, Y;
-    while(fabs(x-xn) > 0.00001){
-        
-        y = y + h*f(x,y);
-        x += h;
-        
-    }
-    printf("x: %f, y: %f\n", x,y);
-
     
+    printf("Enter the value of h : ");
+    scanf("%f", &h);
+
+    printf("Calcualte y(x) where x = : ");
+    scanf("%f", &x);
+
+    float xi = x0;
+    float y = y0;
+    for(float i = xi; i < x; i+=h)
+    {
+        y = y +(h*f(i, y));
+    }
+    printf("The value of y = %f", y);
     return 0;
 }
