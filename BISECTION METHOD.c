198396129d110f55aa45245b29a504721c264b90
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2022-12-16 22:49:25 +0530

    Update BISECTION METHOD.c

--- BISECTION METHOD.c
+++ BISECTION METHOD.c
@@ -1,59 +1,48 @@
-/*   Author: Ayush 
-    Title: Bisection Method
-   Program: Finding real roots of nonlinear
-   equation using Bisection Method*/
-
-
+// cpp program for the bisection formulae
 #include<stdio.h>
-#include<conio.h>
 #include<math.h>
-/*
- Defining equation to be solved.
- Change this equation to solve another problem.
-*/
-#define f(x) cos(x) - x * exp(x)
-
-void main()
+#include<conio.h>
+#define EPSILON 0.01
+double function(double num)
 {
-	 float x0, x1, x2, f0, f1, f2, e;
-	 int step = 1;
-	 clrscr();
-	 /* Inputs */
-	 up:
-	 printf("\nEnter two initial guesses:\n");
-	 scanf("%f%f", &x0, &x1);
-	 printf("Enter tolerable error:\n");
-	 scanf("%f", &e);
-	 /* Calculating Functional Value */
-	 f0 = f(x0);
-	 f1 = f(x1);
-	 /* Checking whether given guesses brackets the root or not. */
-	 if( f0 * f1 > 0.0)
-	 {
-		  printf("Incorrect Initial Guesses.\n");
-		  goto up;
-	 }
-   /* Implementing Bisection Method */
-	 printf("\nStep\t\tx0\t\tx1\t\tx2\t\tf(x2)\n");
-	 do
-	 {
-		  x2 = (x0 + x1)/2;
-		  f2 = f(x2);
-		
-		  printf("%d\t\t%f\t%f\t%f\t%f\n",step, x0, x1, x2, f2);
-		
-		  if( f0 * f2 < 0)
-		  {
-			   x1 = x2;
-			   f1 = f2;
-		  }
-		  else
-		  {
-			   x0 = x2;
-			   f0 = f2;
-		  }
-		  step = step + 1;
-	 }while(fabs(f2)>e);
-	 printf("\nRoot is: %f", x2);
-	 getch();
+    return num*num+2*num-5;
+}
+void bisection(double a,double b)
+{
+    int count=1;
+    int iterate;
+    if(function(a)*function(b)>=0)
+    {
+        printf("you have entereed the wrong interval");
+        return;
+    }
+    printf("enter the number of iterations you really want to perform : ");
+    scanf("%d",&iterate);
+    printf("the entered interval is (%2lf,%2lf)",a,b);
+    double c=a;
+    while(count<=iterate)
+    {
+        c=(a+b)/2;
+        if(function(c)==0.0)break;
+        printf("the root after %d iterations is %lf",count,c);
+        if(function(c)*function(a)<0)
+        b=c;
+        else 
+        a=c;
+        count++;
+    }
+    if((b-a)>=EPSILON)
+    {
+        printf("After the %d iterations the root = %lf \n ",iterate,c);
+    }
+    
+    
+}
+int main()
+{
+    double a,b;
+    printf("enter the initial intervals for the equations");
+    scanf("%lf%lf",&a,&b);
+    bisection(a,b);
+    return 0;
 }
